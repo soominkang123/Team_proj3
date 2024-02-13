095f1b7e8b371fe54d2defdda9b9669f5f68d31e
soomin Kang <tnals3256@naver.com>
2024-02-13 17:13:46 +0900

    최초 커밋

---
+++ src/test/java/com/mysite/Team_Proj3/TeamProj3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.Team_Proj3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProj3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
